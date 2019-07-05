89c600f88cf1d89312acdd9f9a29ef366962a10a
rafidah <alias_rafidah@yahoo.com>
2019-07-05 17:23:16 +0800

    add axios function to post conversation to server

--- index.js
+++ index.js
@@ -48,10 +48,48 @@ io.on('connection', (socket) => {
 
   // Whenever a user send message, we broadcast it to everyone
   socket.on('BROADCAST_MESSAGE', data => {
-    const {username, message, timestamp} = data
-    if (username && message && timestamp && typeof username == 'string' && typeof message == 'string') {
+    console.log(data)
+    const {user_id, message, timestamp, plan_id} = data
+    if (user_id && plan_id && message && timestamp && typeof message == 'string') {
       io.emit('RECEIVE_BROADCAST', data)
-    } else {
+
+  
+          const http = require('http')
+
+          var data = JSON.stringify({ 
+            "plan_id":plan_id,
+            "user_id":user_id,
+            "message":message,
+            "timestamp": timestamp
+          })
+         
+
+          const options = {
+            host: 'localhost',
+            port:5000,
+            path:'/api/v1/chat/messages/'+ plan_id,
+            method: 'POST',
+            headers:{ 'Content-Type' : "application/json"},
+            // body:data
+          }
+
+          const req = http.request(options, (res) => {
+            console.log(`statusCode: ${res.StatusCode}`)
+
+            res.on('data', (d) => {
+              process.stdout.write(d)
+            })
+          })
+
+            req.on('error', (error) => {
+              console.log(error)
+            })
+
+            req.write(data)
+            req.end()
+
+    }
+    else {
       socket.emit('HAS_ERROR', 'BROADCAST_MESSAGE must submit an object with username (string), message (string) and timestamp (number)')
     }
   })
@@ -67,66 +105,15 @@ io.on('connection', (socket) => {
   socket.on("error", () => {
     socket.emit("HAS_ERROR", 'Something went wrong on the server!')
   })
-
-
-  /**
-   * Everything below is for Private Messaging
-   * with Invitation. Optional for students
-   */
-  socket.on('ACCEPT_PRIVATE_CHAT', ({roomName, initiator}) => {
-    console.log(roomName, initiator)
-    socket.join(roomName, () => {
-      io.to(roomName).emit('RECEIVE_PRIVATE_MESSAGE', {
-        sender: {username: 'System Announcement'},
-        message: {
-          type: 'notification',
-          text: `Both of you are here! You may start chatting :)`
-        }
-      })
-    })
-  })
-
-  socket.on('START_PRIVATE_CHAT', ({sender, recipient}) => {
-    if (recipient) {
-      const roomName = `${sender.id}${recipient.id}`
-
-      io.to(recipient.id).emit('RECEIVE_BROADCAST', {
-        message: {
-          type: 'notification',
-          text: `${sender.username} has initiated a private chat with you!`,
-          action: {
-            roomName,
-            initiator: sender,
-            recipient,
-          },
-        }
-      })
-
-      socket.join(roomName, (e) => {
-        io.to(sender.id).emit('RECEIVE_PRIVATE_MESSAGE', {
-          message: {
-            type: 'notification',
-            text: `You have initiated a chat with ${recipient.username}. Please hold!`,
-          }
-        })
-      })
-
-      socket.on('LEAVE_PRIVATE_CHAT', ({leaver}) => {
-        io.to(roomName).emit('RECEIVE_PRIVATE_MESSAGE', {
-          message: {
-            type: 'notification',
-            text: `${leaver.username} left! You are all alone, just leave.`
-          }
-        })
-
-        socket.leave(roomName, () => {
-          console.log(`${sender.username} left!`)
-        })
-      })
-    }
-  })
-
-  socket.on('SEND_PRIVATE_MESSAGE', data => {
-    io.emit('RECEIVE_PRIVATE_MESSAGE', data)
-  })
 })
+
+
+
+
+
+
+
+
+
+
+
